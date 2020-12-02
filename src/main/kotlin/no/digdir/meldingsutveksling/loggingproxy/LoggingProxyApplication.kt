c9f71b2e17c31ee0feb1e92b76a9bffdcb893726
Martin Wam <martin.wam@gmail.com>
2020-12-02 14:00:52 +0100

    rename

--- src/main/kotlin/no/digdir/meldingsutveksling/loggingproxy/LoggingProxyApplication.kt
+++ src/main/kotlin/no/digdir/meldingsutveksling/loggingproxy/LoggingProxyApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KafkaStatusProxyApplication
+class LoggingProxyApplication
 
 fun main(args: Array<String>) {
-	runApplication<KafkaStatusProxyApplication>(*args)
+	runApplication<LoggingProxyApplication>(*args)
 }
